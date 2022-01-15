004f3c88569c73f97182e3cab7c4e448863a1688
anna-ossowska <ossowska.anna1@gmail.com>
2022-01-15 23:57:45 +0100

    Adding global styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
+import React from 'react';
 import { Button } from 'components/common';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+	body {
+		background: white;
+		min-height: 100vh;
+		margin: 0;
+		color: black;
+    font-family: 'Kaushan Script', cursive;
+	}
+`;
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <h1>test</h1>
       <Button>test</Button>
     </>
