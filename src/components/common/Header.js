2a280aeb1f04265c4c09c4265ecb65ba72c37814
anna-ossowska <ossowska.anna1@gmail.com>
2022-01-16 18:54:04 +0100

    Adding media queries and menu icon

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Link as ReactRouterDomLink, useLocation } from 'react-router-dom';
 
@@ -10,19 +10,49 @@ const HeaderWrapper = styled.header`
   padding: 0 16px;
   position: fixed;
   top: 0;
-  background: #f4f4f4;
+  background: #fdd54f;
 `;
 
 const Menu = styled.nav`
-  display: flex;
-  position: relative;
-  width: initial;
-  border-bottom: none;
-  margin: auto 0 auto auto;
+  display: ${(props) => (props.open ? 'block' : 'none')};
   font-family: 'Open Sans', sans-serif;
-  background: none;
-  left: initial;
-  top: initial;
+  position: absolute;
+  width: 100%;
+  top: 60px;
+  left: 0;
+  padding: 8px;
+  box-sizing: border-box;
+  border-bottom: 2px solid #fdd54f;
+  background: white;
+
+  @media only screen and (min-width: 768px) {
+    display: flex;
+    position: relative;
+    background: none;
+    left: initial;
+    top: initial;
+    width: initial;
+    margin: auto 0 auto auto;
+    border-bottom: none;
+  }
+`;
+
+const MobileMenuIcon = styled.div`
+  margin: auto 0 auto auto;
+  width: 25px;
+  min-width: 25px;
+  padding: 5px;
+
+  > div {
+    height: 4px;
+    background: #4f4e4e;
+    margin: 5px 0;
+    width: 100%;
+  }
+
+  @media only screen and (min-width: 768px) {
+    display: none;
+  }
 `;
 
 const Link = ({ isActive, children, ...props }) => {
@@ -30,6 +60,7 @@ const Link = ({ isActive, children, ...props }) => {
 };
 
 const StyledLink = styled(Link)`
+  color: #4f4e4e;
   text-decoration: none;
   padding: 4px 8px;
   display: block;
@@ -41,9 +72,16 @@ const StyledLink = styled(Link)`
 
 export const Header = () => {
   const { pathname } = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <HeaderWrapper>
-      <Menu>
+      <MobileMenuIcon onClick={() => setMenuOpen(!menuOpen)}>
+        <div />
+        <div />
+        <div />
+      </MobileMenuIcon>
+      <Menu open={menuOpen}>
         <StyledLink to="/" isActive={pathname === '/'}>
           Home
         </StyledLink>
