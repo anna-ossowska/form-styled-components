542fa849a92b04cde7ac26487f7452f9ea666b58
anna-ossowska <ossowska.anna1@gmail.com>
2022-01-18 00:00:17 +0100

    Creating the input styled component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,35 @@ import React from 'react';
 import { PageLayout } from 'components/common';
 
 const Home = () => {
-  return <PageLayout>Home</PageLayout>;
+  return (
+    <PageLayout>
+      <h2>Home</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sed
+        esse officiis assumenda error, nobis, temporibus facere, debitis
+        voluptate sapiente nemo totam iusto! Voluptatibus illum dolores
+        voluptates, similique harum quibusdam provident quasi atque molestiae
+        iste fugiat? Ipsa, totam fugiat ex, a animi quasi itaque unde non
+        aperiam temporibus accusamus explicabo doloribus nisi sed nulla, optio
+        sapiente rem cum quam soluta. Eos, quasi atque. Nam aut odit fugiat ad
+        soluta qui hic dolor itaque quasi molestiae velit, neque sint iste,
+        dolore tempore. Iusto aut rerum saepe officiis modi magni quos expedita?
+        Deserunt, inventore. Temporibus molestias odit, error minus dolorum
+        laudantium perspiciatis laborum esse ipsam eius nam explicabo mollitia
+        sit. Iure quisquam eum aut vero repudiandae fugit quibusdam minima
+        eligendi odio voluptatibus, exercitationem dolor perferendis vel
+        repellat sunt asperiores eaque, nisi necessitatibus at pariatur
+        blanditiis! Ullam aliquam doloremque quisquam odio ad? Deleniti ducimus
+        ab saepe exercitationem asperiores, nihil alias id sed iusto amet
+        officia quo dolore, accusamus necessitatibus corporis maiores quisquam
+        unde suscipit. Voluptatibus facere, mollitia doloribus, eos dolorem
+        exercitationem illum minus blanditiis dolore ratione voluptatum
+        explicabo omnis maiores quam. Inventore exercitationem asperiores
+        consequatur molestias corporis alias quas, et, illum atque similique
+        dolorem unde quia sint quaerat soluta voluptas ea, laborum impedit!
+      </p>
+    </PageLayout>
+  );
 };
 
 export default Home;
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import { PageLayout } from 'components/common';
+import React, { useState } from 'react';
+import { PageLayout, Input } from 'components/common';
+import styled from 'styled-components';
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 400px;
+  background: white;
+  border: 1px solid #eee;
+  padding: 16px;
+  box-sizing: border-box;
+  color: black;
+  border-radius: 4px;
+`;
 
 const Login = () => {
-  return <PageLayout>Login</PageLayout>;
+  return (
+    <PageLayout>
+      <h2>Login</h2>
+      <Form>
+        <Input name="username" placeholder="Username" type="text" />
+        <Input name="password" placeholder="Password" type="password" />
+      </Form>
+    </PageLayout>
+  );
 };
 
 export default Login;
