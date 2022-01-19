6b0f7bfa8e62f6875b22fef1e01baa256bdcda64
anna-ossowska <ossowska.anna1@gmail.com>
2022-01-20 00:29:30 +0100

    Implementing the show/hide password toggle

--- src/components/common/PasswordInput.js
+++ src/components/common/PasswordInput.js
@@ -1,7 +1,33 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Header } from 'components/common';
 import { Input } from './Input';
+
+const PasswordInputWrapper = styled.div`
+  display: flex;
+`;
+
+const Text = styled.div`
+  padding-left: 3px;
+`;
+
+const ToggleButton = styled.div`
+  cursor: pointer;
+  height: 40px;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+  font-size: 0.9em;
+  display: flex;
+  justify-content: center;
+  padding: 8px;
+  border-left: 0;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+  background: white;
+  font-weight: bold;
+  user-select: none;
+  color: black;
+  width: 70px;
+`;
 
 const PasswordInputStyled = styled(Input).attrs(() => ({
   type: 'password',
@@ -12,7 +38,21 @@ const PasswordInputStyled = styled(Input).attrs(() => ({
 `;
 
 export const PasswordInput = (props) => {
-  const [showPassword, setshowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
-  return <PasswordInputStyled {...props} />;
+  const clickToggleButtonHandler = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <>
+      <PasswordInputWrapper>
+        <PasswordInputStyled {...props} />
+        <ToggleButton onClick={clickToggleButtonHandler}>
+          {showPassword ? 'Hide' : 'Show'}
+        </ToggleButton>
+      </PasswordInputWrapper>
+      <Text>{showPassword ? props.value : ''}</Text>
+    </>
+  );
 };
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -14,12 +14,32 @@ const Form = styled.form`
 `;
 
 const Login = () => {
+  const [formFields, setFormFields] = useState({ username: '', password: '' });
+
+  const onChangeInputHandler = (e) => {
+    if (e.target.name === 'username')
+      return setFormFields((prev) => ({ ...prev, username: e.target.value }));
+
+    if (e.target.name === 'password')
+      return setFormFields((prev) => ({ ...prev, password: e.target.value }));
+  };
+
   return (
     <PageLayout>
       <h2>Login</h2>
       <Form>
-        <Input name="username" placeholder="Username" type="text" />
-        <PasswordInput name="password" />
+        <Input
+          name="username"
+          placeholder="Username"
+          type="text"
+          value={formFields.username}
+          onChange={onChangeInputHandler}
+        />
+        <PasswordInput
+          name="password"
+          value={formFields.password}
+          onChange={onChangeInputHandler}
+        />
       </Form>
     </PageLayout>
   );
